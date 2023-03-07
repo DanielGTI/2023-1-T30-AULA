d2f46f866ad4bbd59a739088c5046c8129ea1da0
Daniel <daniel.fb.jr@gmail.com>
2023-03-06 21:46:43 -0300

    Exemplo 1

--- POO_01/src/main/java/com/uninove/poo_01/POO_01.java
+++ POO_01/src/main/java/com/uninove/poo_01/POO_01.java
@@ -1,8 +1,24 @@
 package com.uninove.poo_01;
+import java.util.Scanner;
 
 public class POO_01 {
-
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Calculadora de inteiros");
+        //   variaveis
+        int a, b;
+        
+        Scanner ler = new Scanner(System.in);
+        // 1º Valores 
+        System.out.print("Primeiro valor: ");
+        a = ler.nextInt();
+        
+        System.out.print("Segundo valor: ");
+        b = ler.nextInt();
+        
+        System.out.print("\n----------------------\n");
+        System.out.print("\nA soma dos valores é: " + (a+b));
+        System.out.print("\nA subtração dos valores é: " + (a-b));
+        System.out.print("\nA multiplicação dos valores é: " + (a*b));
+        System.out.print("\nA divisão dos valores é: " + (a/b));             
     }
 }
