51dc5ec5f2dad01123d5dfb942d39ece5e5c79e6
Daniel <daniel.fb.jr@gmail.com>
2023-03-13 21:40:00 -0300

    Calculadora IMC

--- POO_01/src/main/java/com/uninove/poo_01/Matematica.java
+++ POO_01/src/main/java/com/uninove/poo_01/Matematica.java
@@ -3,18 +3,35 @@ package com.uninove.poo_01;
 public class Matematica {
     
     //  Atributos locais
-    int a, b;
+    double a, b;
     
     //  Método Construtor
-    public Matematica(int num_a, int num_b){
+    public Matematica(double num_a, double num_b){
         a = num_a;
         b = num_b;
     }
     
     //  Método soma
-    public int somar(){
-        return a + b;
+    public double somar(){
+        return (a + b);
     }
     
+     //  Método soma
+    public double subtrair(){
+        return (a - b);
+    }
     
+     //  Método soma
+    public double multiplicar(){
+        return (a * b);
+    }
+    
+     //  Método soma
+    public double dividir(){
+        return (a / b);
+    }
+    
+    public double calcular_IMC(){
+        return (a / (b * b) );
+    }
 }
--- POO_01/src/main/java/com/uninove/poo_01/POO_01.java
+++ POO_01/src/main/java/com/uninove/poo_01/POO_01.java
@@ -1,19 +1,21 @@
 package com.uninove.poo_01;
+
 import java.util.Scanner;
+
 
 public class POO_01 {
     public static void main(String[] args) {
         System.out.println("Calculadora de inteiros");
         //   variaveis
-        int a, b;
+        double a, b;
         
         Scanner ler = new Scanner(System.in);
         // 1º Valores 
         System.out.print("Primeiro valor: ");
-        a = ler.nextInt();
+        a = ler.nextDouble();
         
         System.out.print("Segundo valor: ");
-        b = ler.nextInt();
+        b = ler.nextDouble();
         
         //  Instanciar a classe Matematica
         Matematica calcular = new Matematica(a, b);
@@ -21,8 +23,22 @@ public class POO_01 {
         
         System.out.print("\n----------------------\n");
         System.out.print("\nA soma dos valores é: " + calcular.somar() );
-        System.out.print("\nA subtração dos valores é: " + (a-b));
-        System.out.print("\nA multiplicação dos valores é: " + (a*b));
-        System.out.print("\nA divisão dos valores é: " + (a/b));             
+        System.out.print("\nA subtração dos valores é: " + calcular.subtrair());
+        System.out.print("\nA multiplicação dos valores é: " + calcular.multiplicar());
+        System.out.print("\nA divisão dos valores é: " + calcular.dividir());             
+    
+
+        System.out.print("\n----------------------\n");
+        System.out.print("Informe o PESO: ");
+        a = ler.nextDouble();
+        
+        System.out.print("Informe a ALTURA: ");
+        b = ler.nextDouble();
+        //  Instanciar a classe Matematica
+        Matematica imc = new Matematica(a, b);
+        
+        System.out.print("\nO valor do IMC é =   " + imc.calcular_IMC() );
+        
+        
     }
 }
