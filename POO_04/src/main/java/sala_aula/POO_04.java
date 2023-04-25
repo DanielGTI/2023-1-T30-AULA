79c2292b2c435c6dabb6d1463ad5d4cb577a06ae
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2023-04-24 22:02:32 -0300

    Metodos Construtores, Acessores e Modificadores

--- POO_04/src/main/java/sala_aula/POO_04.java
+++ POO_04/src/main/java/sala_aula/POO_04.java
@@ -4,13 +4,13 @@ public class POO_04 {
 
     public static void main(String[] args) {
         
-        Pessoa visitante = new Pessoa();
-        
-        visitante.nome = "Marcelo Gomes";
-        visitante.cpf = "123.456.789";
-        visitante.data_Nascimento = "01/05";
-        
+        Pessoa visitante = new Pessoa("Marcelo Gomes", "123.456.789", "01/05");
         visitante.apresentacao();
+        
+        
+        Pessoa visitante_2 = new Pessoa("Daniel", "456.789", "01/06");
+        visitante_2.apresentacao();
+        
         
     }
 }
--- POO_04/src/main/java/sala_aula/Pessoa.java
+++ POO_04/src/main/java/sala_aula/Pessoa.java
@@ -1,24 +1,60 @@
 
 package sala_aula;
-
+/*
+    + publico   public  (todas as classes)
+    - privado   private (somente a classe atual)
+    # protegido (somente a classe atual e subclasses)
+*/
 
 public class Pessoa {
     
-    public String nome;
-    public String cpf;
-    public String data_Nascimento;
+    private String nome;
+    private String cpf;
+    private String data_Nascimento;
     
+    // Método Construtor
+
+    public Pessoa(String nome, String cpf, String data_Nascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.data_Nascimento = data_Nascimento;
+    }
     
     public boolean entrar(){
         return true;
     }
-    
     public boolean sair(){
         return true;
     }
     
     void apresentacao(){
-        System.out.println("Meu nome é " + this.nome);
-        System.out.println("Meu cpf é " + this.cpf);
+        System.out.printf("\n\nMeu nome é " + this.getNome());
+        System.out.printf("\nMeu cpf é " + this.getCpf());
     }
+
+    // Método Acessores e Modificadores
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getData_Nascimento() {
+        return data_Nascimento;
+    }
+
+    public void setData_Nascimento(String data_Nascimento) {
+        this.data_Nascimento = data_Nascimento;
+    }
+    
 }
